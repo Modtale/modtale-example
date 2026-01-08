b2d397d442caf45fce86c567d65533efcff43d4d
Villagers654 <110007851+Villagers654@users.noreply.github.com>
2026-01-08 15:21:30 -0500

    Not really necessary

--- github-actions-plugin/src/main/java/net/example/GithubPlugin.java
+++
@@ -1,7 +0,0 @@
-package net.example;
-
-public class GitHubPlugin {
-    public static void main(String[] args) {
-        System.out.println("This jar was built and published by GitHub Actions!");
-    }
-}
